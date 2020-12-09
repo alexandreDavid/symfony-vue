b2b230bc730cdc76a3e26026b2670b21e167e16f
Alexandre David <alexandre.David42@gmail.com>
2020-12-09 10:36:41 +0100

    first commit

---
+++ assets/VueRoot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input v-model="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
